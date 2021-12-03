5dc418cbeadf311b21d4ab873160f5474f557886
Julia Hartnett <62521409+jhartnett13@users.noreply.github.com>
2021-12-03 16:40:12 -0500

    Add files via upload

--- bankProgram.jl
+++ bankProgram.jl
@@ -2,11 +2,6 @@ using CSV
 using DataFrames
 using Dates
 
-function openFile(user)
-    # code to open the file contatining the users info
-end
-
-
 function signIn()
     ### need to account for what to do if the user doesn't exist in the data set/ the password is incorrect
 
@@ -16,15 +11,9 @@ function signIn()
     df2 = filter(:name => ==(username), df) # find the users name in the data set, make a data set of the users with that name
     println(df2)
     println("Enter your password: ") # get their password
-    ########This is different for if its an int/string password!!!!!!!!!!!!1
     pass = readline()
     userRow = filter(:password => ==(pass), df2)
     println(userRow)
-
-
-    #return(username, )
-
-
 
 
     # once they sign in -- have a second loop to select the other options
@@ -36,9 +25,9 @@ end
 function createAccount()
     # does this function do too much? it makes sense to me to have it all here
     println("Welcome! Thanks for opening a new account.")
-    println("Enter your name: \n") # get the users name
+    println("Enter your name: ")
     name = readline()
-
+    
     password1 = 1 # have the user create a password
     while typeof(password1) != String # the passwords having to be a string makes the dataframe easier to work with
         println("Create a password for your account. Passwords must start with a letter: \n")
@@ -52,51 +41,103 @@ function createAccount()
         password2 = readline()
     end
 
-
     # have the user enter a starting balance for their account
     println("Enter your accounts starting balance: \$")
     startBalance = readline()
-    startBalance = parse(Int64, startBalance) #get an int of the start balance
+    startBalance = parse(Float64, startBalance)  #get a float of the start balance
 
     # create a file that will track their data
     timeStamp = Dates.now()
-    timeStamp = Dates.format(timeStamp, "yyyy-mm-dd HH:MM:SS")
-
-    println(timeStamp)
+    timeStamp = Dates.format(timeStamp, "yyyy-mm-ddHH:MM:SS")
+    timeStamp = replace(timeStamp, ":"=>"-")
 
     userFileName = name * timeStamp *".csv"
+    
+    ##########  WRITE THIS TO THE FILE OF ALL THE USERS #########################
+    df = CSV.read("userAccounts.csv", DataFrame)
+    println(df)
+    push!(df, [name password1 startBalance userFileName])  ## work around is to have the first line be a bunch of nonsense
+    CSV.write("userAccounts.csv", df)
+    close("userAccounts.csv")
 
-    println(userFileName)
-
-    # create an actual .csv file
-
-    # write their data to the user file
-
+    # create a df of the users info
+    date3 = Dates.today()
+    df = DataFrame(Date = [date3], TransactionAmount = [startBalance], Category = ["Initial Deposit"], AccountTotal = [startBalance] )
+    println(df)
 
 
-    # -- after signing in, allow the user to do other things without having to quit and go through the whole loop again
-
+    # create a file for the specific user
+    touch(userFileName)
+    openFile = open(userFileName, "w")
+    CSV.write(userFileName, df)
+    close(userFileName)
 end
 
-function useProgram()
+
+function useProgram() # might take in the users file name? so that cna be updated/data can be found and spent
+
     #once a user signs in, they will be directed here so signing in isn't an option anymore
     #once they exit from this loop they will be back at the signin /exit screen
 
+    kg = "yes"
+    println("What would you like to do? ")
+    while kg == "yes"
+        println("1. Enter a transaction (spending/withdrawl)")
+        println("2. Enter a deposit")          # should transaction and deposit be the same thing???
+        println("3. Set a Budget")
+        println("4. View Statistics")
+        println("5. Quit")
+        choice = readline()
+        choice = parse(Int64, choice)
+        if choice == 1
+            enterTransaction()
+        elseif choice == 2
+            enterDeposit()
+        elseif choice == 3
+            setBudget()
+        elseif choice == 4
+            getStats()
+        elseif choice == 5
+            global kg = "no"
+
+        end
+end
+
+
 
 end
 
 
-function enterTransaction()
-    # i don't think this function will take input -- maybe the user's balance?
-    # questions will be asked within the function
+function enterTransaction() # take in users file
+    kg = "yes"
+    while kg == "yes"
+        #enter the amount of the transaction
+
+        #ask for the type of the transaction
+
+        # if withdrawl then subtract the value from their total amount
+
+
+
+
+    end
+
+
+
+    println("Enter the amount you spent: \$")
+    amount = readline()
+    amount = parse(Float64, amount)
+    println(amount)
+
     # ask the user for the transaction date, amount, and category. other data TBD
     # write the data to a file
     # keeping track of the balance
 end
 
-function enterDeposit()
-    # no input --- maybe the account balancce so that can be updated
-    # questions will be asked within the function
+function enterDeposit() # take in the users file
+
+
+
     # ask the user for the deposit amount and date
     # update the account balance
 end 
@@ -104,6 +145,10 @@ end
 function setBudget()
     # will ask the user to select the category and set the percent of their budget to make the category
     # percents can not exceed 100%
+    # will need to adjust based on the users balance
+
+    # how much would you like to spend each month
+    # 
 end 
 
 function getStats()
@@ -111,19 +156,8 @@ function getStats()
     # viewing spending by category, looking at data within a particular month, etc.
 end
 
-bank_users = [] #empty list of bank users, will be used to check for accounts? 
 
-mutable struct User # decided to make a User struct to be able to keep track of different attributes
-    # i might not need this if i'm using a data frame/csv to keep track of data?????????????
-    name::String
-    accountBalance::Float64
-    password::String
-    #accountFile::String  -- store the file as a string that can be read/opened?? 
-end
-
-
-
-
+## main loop
 kg = "yes"
 
 println("Welcome! What would you like to do? ")
